7015f8a5de45c6de7161811bc4f3c74dc45e7641
Demmoman6666 <corey@heduc8.co.uk>
2025-08-22 18:36:15 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // add any options you need here
-  // experimental: { serverActions: true },
-};
-
-export default nextConfig;
+  reactStrictMode: false,
+}
+export default nextConfig
