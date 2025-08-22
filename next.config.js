600f718fc33456c542b237fddf585bbfdd9ee267
Demmoman6666 <corey@heduc8.co.uk>
2025-08-22 17:48:41 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: { allowedOrigins: ["*"] }
-  }
+  reactStrictMode: true,
+  // output: 'standalone',
 };
+
 module.exports = nextConfig;
