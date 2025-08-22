74e8636309eb7e9dd7a8058aefe018c70416961d
Demmoman6666 <corey@heduc8.co.uk>
2025-08-22 18:19:33 +0100

    Update and rename next.config.js to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // add any options you need here
+  // experimental: { serverActions: true },
+};
+
+export default nextConfig;
