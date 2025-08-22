79c31d7edd432c8f66980c2b9f9c67be8ad785df
Demmoman6666 <corey@heduc8.co.uk>
2025-08-22 18:49:52 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
-import './globals.css'
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'SBP CRM',
-  description: 'Basic CRM',
-}
+// app/layout.tsx
+export const metadata = { title: 'SBP CRM' };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
